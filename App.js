6397808ce827ca5abf38bc533e1a51fbe5e74ce4
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2024-01-14 22:01:20 +0200

    Shopping list ready.

--- App.js
+++ App.js
@@ -1,20 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState([]);
+  const [listItem, setListItem] = useState("");
+
+  const handleInputChange = (text) => {
+    setListItem(text);
+  };
+  const addToList = () => {
+    setShoppingList((prevList) => [...prevList, listItem]);
+    setListItem("");
+  };
+  const clearList = () => {
+    setShoppingList([]);
+    setListItem("");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <TextInput
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+            height: 40,
+            width: "80%",
+            marginLeft: "10%",
+          }}
+          value={listItem}
+          onChangeText={(text) => handleInputChange(text)}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            marginBottom: 10,
+            height: 50,
+          }}
+        >
+          <Pressable
+            onPress={() => addToList()}
+            style={{
+              backgroundColor: "green",
+              height: 30,
+              width: "20%",
+
+              marginTop: "3%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ color: "white" }}>Add</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => clearList()}
+            style={{
+              backgroundColor: "red",
+              height: 30,
+              width: "20%",
+              marginTop: "3%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text>Clear List</Text>
+          </Pressable>
+        </View>
+        <Text
+          style={{ color: "blue", marginLeft: "auto", marginRight: "auto" }}
+        >
+          Shopping List
+        </Text>
+
+        <FlatList
+          style={{
+            marginLeft: "auto",
+            marginRight: "auto",
+            flex: 1,
+            marginTop: 5,
+          }}
+          data={shoppingList}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "stretch",
+    justifyContent: "center",
+    marginTop: "30%",
   },
 });
